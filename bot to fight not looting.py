7d5258191ade789bab973d4102c1dcbea0adb055
Maxellius <maxell41852963@gmail.com>
2022-12-13 17:02:35 +0000

    fixed the problem of finding a mob in a state of rest

--- bot to fight not looting.py
+++ bot to fight not looting.py
@@ -37,12 +37,14 @@ def save_main_picture(name_picture: str, top: int, left: int, wight: int, height
 last_time = time.time()
 
 main_png = 'main.png'
+chat_png = 'chat.png'
 loot_png = 'loot.png'
 empty_enemy_heals_bar_fragment = './fragments/empty_heals_bar_fragment.png'
 heal_bar_png = 'heal.png'
 heal_bar_fragment = './fragments/heal_fragment.png'
 distance_to_loot_1 = 'distance_to_loot_1.png'
 distance_to_loot_2 = 'distance_to_loot_2.png'
+error_chat_fragment = './fragments/error_chat_fragment.png'
 
 iteration = 1
 while True:
@@ -111,13 +113,19 @@ while True:
                                                              0.98) is not None:
                     break
                 pyautogui.press('6')
+                save_main_picture(chat_png, top=539, left=19, wight=170, height=125)
+                if find_coordinates_template_img_in_main_img(chat_png, error_chat_fragment,
+                                                             0.9) is not None:
+                    pyautogui.press('0')
                 save_main_picture(main_png, top=32, left=596, wight=145, height=14)
+
 
     else:
         # print(f'Lotting {datetime.datetime.today().strftime("%H:%M:%S")}')
         # save_main_picture(loot_png, top=134, left=266, wight=10, height=10)
         # a = 1
         # while find_coordinates_template_img_in_main_img(loot_png, './fragments/loot_fragment.png', 0.7) is None:
+        #     time.sleep(0.5)
         #     pyautogui.press('c')
         #     time.sleep(1)
         #     if a % 10 == 0:
@@ -128,26 +136,26 @@ while True:
         #         if find_coordinates_template_img_in_main_img(distance_to_loot_1, distance_to_loot_2, 0.9):
         #             pyautogui.press('esc')
         #             break
-
-            # save_main_picture('main.png', top=32, left=596, wight=145, height=14)
-            # if find_coordinates_template_img_in_main_img(main_png, empty_enemy_heals_bar_fragment,
-            #                                              0.7) is None:
-            #     break
-            # a += 1
-            # save_main_picture(loot_png, top=134, left=266, wight=10, height=10)
+        #
+        #     save_main_picture('main.png', top=32, left=596, wight=145, height=14)
+        #     if find_coordinates_template_img_in_main_img(main_png, empty_enemy_heals_bar_fragment,
+        #                                                  0.7) is None:
+        #         break
+        #     a += 1
+        #     save_main_picture(loot_png, top=134, left=266, wight=10, height=10)
         # else:
-            # pyautogui.keyDown('shift')
-            # pyautogui.keyDown('c')
-            # time.sleep(0.5)
-            # pyautogui.keyUp('shift')
-            # pyautogui.keyUp('c')
+        #     pyautogui.keyDown('shift')
+        #     pyautogui.keyDown('c')
+        #     time.sleep(0.5)
+        #     pyautogui.keyUp('shift')
+        #     pyautogui.keyUp('c')
         print(f'Healing {datetime.datetime.today().strftime("%H:%M:%S")}')
-        pyautogui.press('esc')
+        # pyautogui.press('esc')
         save_main_picture(heal_bar_png, top=711, left=297, wight=141, height=10)
-        if find_coordinates_template_img_in_main_img(heal_bar_png, heal_bar_fragment, 0.60) is None:
+        if find_coordinates_template_img_in_main_img(heal_bar_png, heal_bar_fragment, 0.66) is None:
             time.sleep(0.5)
             pyautogui.press('0')
-            while find_coordinates_template_img_in_main_img(heal_bar_png, heal_bar_fragment, 0.60) is None:
+            while find_coordinates_template_img_in_main_img(heal_bar_png, heal_bar_fragment, 0.66) is None:
                 save_main_picture(heal_bar_png, top=711, left=297, wight=141, height=10)
             else:
                 time.sleep(0.5)
